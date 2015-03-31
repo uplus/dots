626be8e4b8ed59bb57baa19e41f74a5438961a66
YuutoIto <uplus.e10@gmail.com>
2015-04-01 03:04:03 +0900

    make zsh setup command

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,5 +1,6 @@
 for name in `\ls`; do
 	if [ -f $name ] && [ $name != `basename $0` ] && [ $name != "README.md" ]; then
 		ln -svi "$PWD/$name" "$HOME/.$name"
+	  # echo "$PWD/$name" "$HOME/.$name"
 	fi
 done
