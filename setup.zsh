eee53b883c8c94a189e35cbdb82026aacd30a0ba
YuutoIto <uplus.e10@gmail.com>
2015-04-09 02:54:45 +0900

    change return to exit

--- setup.zsh
+++ setup.zsh
@@ -3,12 +3,9 @@
 which git > /dev/null 
 if [ $? -ne 0 ]; then
   echo "please run again after install the git" >&2 
-  return 1
+  exit 1
 fi
-ln -s $PWD/gitconfig $HOME/.gitconfig
-
-return 0
-exit
+ln -svi $PWD/gitconfig $HOME/.gitconfig
 
 #NeoBundle
 mkdir -p ~/.vim/bundle
@@ -20,7 +17,5 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-#実行元にも影響
-exec $SHELL -l
 
 #その他にもvimビルドしたりいろいろインストールしたりする予定
