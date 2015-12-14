2e283f5484cac4f3347cacf07de0139052f1dc2c
u10e10 <uplus.e10@gmail.com>
2015-12-15 01:46:28 +0900

    refactoring: change `` to $().

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -eu
-current=$(cd `dirname $0` && pwd)
-for name in `\ls -F $current/ | egrep -v ".*/|.*\*|README\..*"`; do
+current=$(cd $(dirname $0) && pwd)
+for name in $(\ls -F $current/ | egrep -v ".*/|.*\*|README\..*"); do
   ln -svi "$current/$name" "$HOME/.$name"
 done
 
 [[ ! -d $HOME/.zsh ]] && mkdir $HOME/.zsh
 [[ ! -d $HOME/.zsh/antigen ]] && git clone https://github.com/zsh-users/antigen.git $HOME/.zsh/antigen
 
-os_local_file=$HOME/.zsh.`uname | tr '[:upper]' '[:lower]'`
+os_local_file="$HOME/.zsh.$(uname | tr '[:upper]' '[:lower]')"
 
 case $OSTYPE in
 linux*)
