a29e33e39297faf0aea1ecfd5f8fc55479ab08ae
u10e10 <uplus.e10@gmail.com>
2016-01-10 19:46:57 +0900

    update: change percol color config.2

--- percol.rc.py
+++ percol.rc.py
@@ -1,7 +1,7 @@
 # Run command file for percol
 percol.view.PROMPT  = r"<bold><cyan>QUERY </cyan>[a]:</bold> %q"
-percol.view.CANDIDATES_LINE_SELECTED = ("on_magenta", "white")
-percol.view.CANDIDATES_LINE_QUERY    = ("green", "bold")
+percol.view.CANDIDATES_LINE_SELECTED = ("on_blue", "white")
+percol.view.CANDIDATES_LINE_QUERY    = ("cyan", "reverse")
 
 # Display finder name in RPROMPT
 percol.view.prompt_replacees["F"] = lambda self, **args: self.model.finder.get_name()
@@ -10,20 +10,21 @@ percol.view.RPROMPT = r"\<%F\> (%i/%I) [%n/%N]"
 percol.import_keymap({
     "C-h" : lambda percol: percol.command.delete_backward_char(),
     "C-d" : lambda percol: percol.command.delete_forward_char(),
-    "C-k" : lambda percol: percol.command.kill_end_of_line(),
-    "C-y" : lambda percol: percol.command.yank(),
     "C-a" : lambda percol: percol.command.beginning_of_line(),
     "C-e" : lambda percol: percol.command.end_of_line(),
-    "C-b" : lambda percol: percol.command.backward_char(),
     "C-f" : lambda percol: percol.command.forward_char(),
+    "C-b" : lambda percol: percol.command.backward_char(),
     "C-n" : lambda percol: percol.command.select_next(),
     "C-p" : lambda percol: percol.command.select_previous(),
+    "C-k" : lambda percol: percol.command.kill_end_of_line(),
+
+    "C-y" : lambda percol: percol.command.yank(),
     "C-v" : lambda percol: percol.command.select_next_page(),
     "M-v" : lambda percol: percol.command.select_previous_page(),
     "M-<" : lambda percol: percol.command.select_top(),
     "M->" : lambda percol: percol.command.select_bottom(),
+
     "C-m" : lambda percol: percol.finish(),
     "C-j" : lambda percol: percol.finish(),
     "C-g" : lambda percol: percol.cancel(),
-    "C-q" : lambda percol: percol.cancel(),
 })
