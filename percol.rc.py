28c7552dc13714d3f451de515aa5ddf0001d424c
u10e10 <uplus.e10@gmail.com>
2016-01-29 00:25:31 +0900

    enable migemo in percol.

--- percol.rc.py
+++ percol.rc.py
@@ -1,3 +1,8 @@
+from percol.finder import FinderMultiQueryMigemo
+
+FinderMultiQueryMigemo.dictionary_path = '/usr/share/cmigemo/utf-8/migemo-dict'
+FinderMultiQueryMigemo.minimum_query_length = 1
+
 # Run command file for percol
 percol.view.PROMPT  = r"<bold><cyan>QUERY </cyan>[a]:</bold> %q"
 percol.view.CANDIDATES_LINE_SELECTED = ("on_blue", "white")
@@ -27,4 +32,8 @@ percol.import_keymap({
     "C-m" : lambda percol: percol.finish(),
     "C-j" : lambda percol: percol.finish(),
     "C-g" : lambda percol: percol.cancel(),
+
+    "M-c" : lambda percol: percol.command.toggle_case_sensitive(),
+    "M-m" : lambda percol: percol.command.toggle_finder(FinderMultiQueryMigemo),
+    "M-r" : lambda percol: percol.command.toggle_finder(FinderMultiQueryRegex)
 })
