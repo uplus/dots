ef1d164a011ea1c1766bd4a5014fdbe5d72a5dfa
u10e10 <uplus.e10@gmail.com>
2016-03-24 22:28:25 +0900

    source zsh config files directly(don't use symbolic link).

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,22 +1,8 @@
 #!/bin/bash -eu
 current="$(cd -- "$(dirname -- "${BASH_SOURCE}")" && pwd)"
-for name in $(\ls -F $current/ | egrep -v ".*/|.*\*|README\..*"); do
-  ln -svi "$current/$name" "$HOME/.$name"
-done
 
-[[ ! -d $HOME/.zsh ]] && mkdir $HOME/.zsh
+ln -svi "$current/zshenv" "$HOME/.zshenv"
+ln -svi "$current/zshrc" "$HOME/.zshrc"
 
-os_local_file="$HOME/.zsh.$(uname | tr '[:upper]' '[:lower]')"
-
-case $OSTYPE in
-linux*)
-  rm_file=darwin
-  ;;
-darwin*)
-  rm_file=linux
-  ;;
-esac
-
-[[ -L $HOME/.zshrc.$rm_file ]] && rm $HOME/.zshrc.$rm_file
-
+[[ ! -d $HOME/.zsh ]] && mkdir -p $HOME/.zsh
 [[ ! $SHELL =~ '/zsh' ]] && chsh -s /bin/zsh
