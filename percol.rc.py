12a97f470c1df0f9a8d012e69ac515eddc799a89
u10e10 <uplus.e10@gmail.com>
2016-09-05 18:37:11 +0900

    Change percol color.

--- percol.rc.py
+++ percol.rc.py
@@ -5,8 +5,8 @@ FinderMultiQueryMigemo.minimum_query_length = 2
 
 # Run command file for percol
 percol.view.PROMPT  = r"<bold><cyan>QUERY </cyan>[a]:</bold> %q"
-percol.view.CANDIDATES_LINE_SELECTED = ("on_blue", "white")
-percol.view.CANDIDATES_LINE_QUERY    = ("cyan", "reverse")
+percol.view.CANDIDATES_LINE_SELECTED = ("on_blue", "black")
+percol.view.CANDIDATES_LINE_QUERY    = ("on_cyan", "black")
 
 # Display finder name in RPROMPT
 percol.view.prompt_replacees["F"] = lambda self, **args: self.model.finder.get_name()
