9f628076c2f1189719283297cee1adfa248c77ee
u10e10 <uplus.e10@gmail.com>
2016-11-23 12:59:10 +0900

    bcho: Fix hex_align bug.

--- utils/bcho.rb
+++ utils/bcho.rb
@@ -2,24 +2,41 @@
 
 class String
   def int?
-    Integer(self) && true rescue false
+    Integer(self)
+  rescue # special
+    Integer('0x'+self)
+    true
+  rescue
+    false
+  end
+
+  def to_bin
+    self.to_i(16).chr
+  end
+
+  def hex_strip
+    self.dup.hex_strip!
+  end
+
+  def hex_strip!
+    self.sub!(/^0x|h$/, '')
   end
 end
 
 def hex_align(hex_str)
+  hex_str.hex_strip!
   hex_str.insert(0, '0') if hex_str.size.odd?
   hex_str
 end
 
-def little(hex_str)
-  hex_str = hex_strip(hex_str)
+def to_little(hex_str)
   hex_str = hex_align(hex_str)
-  return hex_str.scan(/../).map{|s| s.to_i(16).chr}.reverse.join
+  return hex_str.scan(/../).map(&:to_bin).reverse.join
 end
 
 ARGV.map do |str|
   if str.int?
-    print little(str)
+    print to_little(str)
   else
     str += "\0" if str !~ /\0$/
     print str
