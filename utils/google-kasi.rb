58e1f10c6b7a67b9e4d2b9dbb857a4eea6f653c3
u10e10 <uplus.e10@gmail.com>
2016-12-11 03:08:38 +0900

    utils/google-kasi: Add Google.

--- utils/google-kasi.rb
+++ utils/google-kasi.rb
@@ -1,67 +1,12 @@
 #!/usr/bin/env ruby
-require 'open-uri'
-require 'nokogiri'
-require 'tty-pager'
 require 'pry'
 
-def parse(url_str, opt={})
-  charset = nil
-  opt[:redirect] = false
-  html = open(URI.encode(url_str), opt) do |f|
-    charset = f.charset
-    f.read
-  end
-  Nokogiri::HTML.parse(html, nil, charset)
+require_relative './lyrics.rb'
 
-rescue OpenURI::HTTPRedirect => e
-  # for http -> https redirect
-  puts 'redirect: %s -> %s' % [url_str, e.uri.to_s]
-  url_str = e.uri.to_s
-  retry
-end
-
-def lyrics(doc)
-  js_text = doc.css('.center > script')[0].text
-  html_text = js_text.split("\n").grep(/^\s*var\s*lyrics\s*=/)[0]
-  text = Nokogiri::HTML.parse(html_text).xpath('//p').inner_html.gsub('<br>', "\n")
-  text.sub!(/\Avar\s*lyrics\s*=\s*'/, '')
-  text.sub!(/';\z/, '')
-  text
-end
-
-
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-def info(doc)
-  table    = doc.at_css('.person_list_and_other_contents')
-  title    = table.at_css('h1').text.strip
-  person   = table.at_css('.person_list').css('a').text.gsub(/\s+/, ' ').strip
-  category = table.at_css('.other_list').at_css('tr > td').text.gsub(/\s+/, ' ').strip
-  "#{title} -- #{category} - #{person}"
-end
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def puts_color(color_num, str)
-  puts "\e[#{color_num}m#{str}\e[0m"
-end
-
-def is_item?(url_str)
-  uri = URI.parse(url_str)
-  /^\/item/ =~ uri.path
-end
-
-def puts_g(child)
-  cite = 'http://'+child.at_css('cite').text
-
-  unless is_item?(cite)
-    title = child.at_css('.r > a').text
-    puts_color 33, "%s(%s)\n" % [title, cite]
-    return nil
-  end
-
-  doc = parse(cite)
-  puts_color 95, info(doc)
-  puts lyrics(doc).gsub("\n", ' ')[0..60], ''
-  cite
+  puts "\e[38;5;#{color_num}m#{str}\e[00m"
 end
 
 def pager
@@ -73,31 +18,81 @@ ensure
   IO.popen(['less', opt], 'w'){|f| f.puts out.string }
 end
 
+def input_num(str)
+  print str
+  gets&.to_i
+rescue Interrupt
+  nil
+end
 
-begin
 
-url = 'https://www.google.co.jp/search?q=site:www.kasi-time.com+intitle:%s' % 'ライオン'
-doc = parse(url)
-g = doc.css('#search .g')
+module Google
+  @@baseurl = 'https://www.google.co.jp/search?q=site:www.kasi-time.com+intitle:%s'.freeze
+  module_function
 
-cands = pager do
-  g.map.with_index do |child, i|
-    print "#{i} "
-    puts_g child
+  def url(query)
+    @@baseurl % query
+  end
+
+  def doc(url_str)
+    doc = parse(url_str)
+  end
+
+  def cands(doc)
+    doc.css('#search .g').map do |cand|
+      title = cand.at_css('.r > a').text
+      url = 'http://'+cand.at_css('cite').text
+      description = cand.at_css('span').text
+      {title: title, url: url, description: description}
+    end
+  end
+
+  def select(cands)
+    case cands.size
+    when 0
+      nil
+    when 1
+      cands[0]
+    else
+      pager do
+        cands.each_with_index do |cand, i|
+          print "#{i} "
+          yield cand
+        end
+      end
+
+      num = input_num("[0..#{cands.size-1}]> ")
+      cands[num] rescue nil
+    end
+  end
+
+  def search(query)
+    cands(doc(url(query)))
   end
 end
 
+def put_cand(cand)
+  unless /^\/item/ =~ URI.parse(cand[:url]).path
+    puts_color 184, "%{title}(%{url})\n" % cand
+    return
+  end
 
-print "number>> "
-num = gets.to_i
+  doc = Kasitime.doc(cand[:url])
+  puts_color 111, Kasitime.info_str(doc)
+  puts Kasitime.lyrics(doc).gsub("\n", ' ')[0..60], ''
+end
 
-puts cands[num]
-puts lyrics(parse(cands[num]))
+begin
+  cands = Google.search('ライオン')
+  cand = Google.select(cands, &method(:put_cand))
 
-binding.pry
+  # select  [lyrics[save, out], thumbnail, all]
+  puts Kasitime.lyrics(parse(cand[:url]))
+
 rescue => e
   puts "Error"
-  p e
+  p e, e.backtrace, e.message
+ensure
   binding.pry
 end
-# description = g[0].css('span').text
+
