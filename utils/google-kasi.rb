36d560b80c7fff9caba9e6291e10ede44165946a
u10e10 <uplus.e10@gmail.com>
2016-12-10 20:59:25 +0900

    Update utils/google-kasi.rb

--- utils/google-kasi.rb
+++ utils/google-kasi.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'open-uri'
 require 'nokogiri'
+require 'tty-pager'
 require 'pry'
 
 def parse(url_str, opt={})
@@ -19,15 +20,17 @@ rescue OpenURI::HTTPRedirect => e
   retry
 end
 
+def lyrics(doc)
+  js_text = doc.css('.center > script')[0].text
+  html_text = js_text.split("\n").grep(/^\s*var\s*lyrics\s*=/)[0]
+  text = Nokogiri::HTML.parse(html_text).xpath('//p').inner_html.gsub('<br>', "\n")
+  text.sub!(/\Avar\s*lyrics\s*=\s*'/, '')
+  text.sub!(/';\z/, '')
+  text
+end
 
-  def lyrics(doc) # TODO delete
-    js_text = doc.css('.center > script')[0].text
-    html_text = js_text.split("\n").grep(/^\s*var\s*lyrics\s*=/)[0]
-    text = Nokogiri::HTML.parse(html_text).xpath('//p').inner_html.gsub('<br>', "\n")
-    text.sub!(/\Avar\s*lyrics\s*=\s*'/, '')
-    text.sub!(/';\z/, '')
-    text
-  end
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def info(doc)
   table    = doc.at_css('.person_list_and_other_contents')
@@ -37,17 +40,8 @@ def info(doc)
   "#{title} -- #{category} - #{person}"
 end
 
-# site:www.kasi-time.com intitle:ノーポイッ
-url = 'https://www.google.co.jp/search?q=site:www.kasi-time.com+intitle:%s' % 'ライオン'
-doc = parse(url)
-
-search = doc.css('#search')
-g = search.css('.g')
-
 def puts_color(color_num, str)
-  print "\e[%dm" % color_num
-  puts str
-  print "\e[0m"
+  puts "\e[#{color_num}m#{str}\e[0m"
 end
 
 def is_item?(url_str)
@@ -55,31 +49,55 @@ def is_item?(url_str)
   /^\/item/ =~ uri.path
 end
 
-begin
-
-g.each do |child|
+def puts_g(child)
   cite = 'http://'+child.at_css('cite').text
 
   unless is_item?(cite)
     title = child.at_css('.r > a').text
-    puts_color 33, '%s(%s)' % [title, cite]
-    next
+    puts_color 33, "%s(%s)\n" % [title, cite]
+    return nil
   end
 
   doc = parse(cite)
   puts_color 95, info(doc)
-  puts lyrics(doc).gsub("\n", ' ')[0..60]
-  puts
+  puts lyrics(doc).gsub("\n", ' ')[0..60], ''
+  cite
 end
 
+def pager
+  $stdout = out = StringIO.new
+  return yield
+ensure
+  $stdout = STDOUT
+  opt='--RAW-CONTROL-CHARS --quit-if-one-screen --no-init'
+  IO.popen(['less', opt], 'w'){|f| f.puts out.string }
+end
+
+
+begin
+
+url = 'https://www.google.co.jp/search?q=site:www.kasi-time.com+intitle:%s' % 'ライオン'
+doc = parse(url)
+g = doc.css('#search .g')
+
+cands = pager do
+  g.map.with_index do |child, i|
+    print "#{i} "
+    puts_g child
+  end
+end
+
+
+print "number>> "
+num = gets.to_i
+
+puts cands[num]
+puts lyrics(parse(cands[num]))
 
 binding.pry
 rescue => e
+  puts "Error"
   p e
   binding.pry
 end
-
-# title = g[0].css('.r > a').text
-# title = g[0].at_css('.r > a').text
-# cite = g[0].css('cite').text
 # description = g[0].css('span').text
