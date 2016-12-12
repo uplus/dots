840197e3ea7458e88f2003842c46af7bbdc79c6c
u10e10 <uplus.e10@gmail.com>
2016-12-12 12:03:07 +0900

    utils/play-music.zsh: Allow multi select.

--- utils/play-music.zsh
+++ utils/play-music.zsh
@@ -1,20 +1,18 @@
 #!/usr/bin/env zsh
 set -ue
-local music_dir rc_file ignore music
 export LC_ALL=en_US.UTF-8
-music_dir="$HOME/Music"
-rc_file="$HOME/.play-music-rc"
+local -r music_dir="$HOME/Music"
+local -r rc_file="$HOME/.play-music-rc"
+[[ -f $rc_file ]] && local -r ignore="grep -vif '${rc_file}' |"
 
-filter='percol --match-method=migemo'
+local -r filter='percol --match-method=migemo'
 # if type migemogrep &>! /dev/null; then
 #   filter='peco --initial-filter Migemo'
 # else
 #   filter='percol --match-method=migemo'
 # fi
 
-[[ -f $rc_file ]] && ignore="grep -vif '${rc_file}' |"
+local -r songs=$(eval "cd '${music_dir}' && find -L -type f | ${ignore:-} sed 's|^\./||' | ${filter}")
+[[ -z $songs ]] && exit
 
-music=$(eval "cd '${music_dir}' && find -L -type f | ${ignore} sed 's|^\./||' | ${filter}")
-
-[[ -z $music ]] && exit
-vlc --daemon "${music_dir}/${music}" 2>/dev/null
+echo "${songs}" | xargs -I{@} vlc --daemon "${music_dir}/{@}" 2>/dev/null
