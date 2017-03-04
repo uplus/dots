a4c254522de9b410772222dc43ea71389677ea72
u10e10 <uplus.e10@gmail.com>
2017-03-05 04:06:07 +0900

    Update utils/fsb-write.rb

--- utils/fsb-write.rb
+++ utils/fsb-write.rb
@@ -9,9 +9,25 @@ def unpack(hex_str)
   hex_str.unpack('V')[0]
 end
 
-data_value = 0x8048691    # data to be written
-dest = 0x080499fc   # destination base
-pos = 6             # スタックの位置
+if ARGV.size < 3
+  warn "#{File.basename($0)} pos dest_hex data_hex"
+  warn "Don't out newline"
+  exit 1
+end
+
+$VERBOSE = nil unless ENV['DEBUG']
+
+# スタックの位置
+pos = ARGV[0].to_i
+
+# destination base
+# dest = 0x080499fc
+dest = ARGV[1].to_i(16)
+
+# data to be written
+# data_value = 0x8048691
+data_value = ARGV[2].to_i(16)
+
 
 # 下位から書き込むために分割&reverse
 data    = data_value.to_s(16).reverse.scan(/.{1,4}/).map{|s| s.reverse.to_i(16)}
