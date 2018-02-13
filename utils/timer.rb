73aa5f38d5d242c727f0cdf9baf4f64c27f30495
uplus <uplus.e10@gmail.com>
2018-02-14 00:01:20 +0900

    update

--- utils/timer.rb
+++ utils/timer.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 
+finish_time = ARGV[0] || '^'
+
 loop do
   time = Time.now.strftime('%T.%1L')
   print("\r\e[0J#{time}")
   # \e[H\e[2J
 
-  if /${1:-^}$/ =~ time
+  if /#{finish_time}$/ =~ time
     system('xdotool mousedown 1')
-    sleep(0.02)
+    sleep(0.025)
     system('xdotool mouseup 1')
     print("\r\e[0J#{Time.now.strftime('%T.%3L')}")
     break
