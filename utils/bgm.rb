a9d227e5ce5c65bd15b99b51216d48ccda33e382
uplus <uplus.e10@gmail.com>
2018-03-11 02:01:11 +0900

    update

--- utils/bgm.rb
+++ utils/bgm.rb
@@ -1,13 +1,8 @@
 #!/usr/bin/env ruby
-=begin
-  -n {NUM}  Number
-  -d {DIR}  Directory default is ~/Music/
-  -p {play} Player   default is vlc
-=end
-
 require 'find'
 require 'optparse'
 require 'shellwords'
+require 'open3'
 
 class Hash
   def symbolize_keys
@@ -46,6 +41,13 @@ end
 IGNORE_FILE = File.join(Dir.home, '.bgm_ignore').freeze
 IGNORE_PATTERNS = (File.file?(IGNORE_FILE)? regexps_from_file(IGNORE_FILE) : []).freeze
 
+=begin
+  -n {NUM}  Number
+  -d {DIR}  Directory default is ~/Music/
+  -p {play} Player   default is vlc
+=end
+
+
 option         = get_option()
 all_files      = getmusics(option[:dir])
 selected_files = music_select(all_files, option[:num])
@@ -53,9 +55,9 @@ escaped_files  = selected_files.map {|f| f.shellescape }
 puts selected_files.sort
 
 begin
-  system "%s %s >/dev/null 2>&1 &" % [option[:play], escaped_files.join(' ')]
+  Open3.capture3("%s %s >/dev/null 2>&1 &" % [option[:play], escaped_files.join(' ')])
 rescue => e
   puts e, e.backtrace
   puts "\n#arg files"
-  selected_files.zip(escaped_files) {|s,e| p s, e }
+  selected_files.zip(escaped_files) {|str,esc| p str, esc }
 end
