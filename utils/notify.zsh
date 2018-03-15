7e83be993567f996f918809242a2cefcdf193057
uplus <uplus.e10@gmail.com>
2018-03-15 22:13:39 +0900

    update

--- utils/notify.zsh
+++ utils/notify.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+if [[ ${1:-} = '--sound' ]]; then
+  play /usr/share/sounds/Oxygen-Sys-Log-In-Short.ogg >&! /dev/null <&- &!
+  shift
+fi
+
 message="${1:?message}"
 notify-send --urgency=low --icon="${HOME}/.face.icon" --app-name='' "${message}"
-play /usr/share/sounds/Oxygen-Sys-Log-In-Short.ogg >&! /dev/null <&- &!
