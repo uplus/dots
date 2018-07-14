99f43b98784f0e39fa3de1f205c53ea5306b77fc
uplus <uplus.e10@gmail.com>
2018-07-14 15:44:57 +0900

    update

--- utils/ssh-tunnel.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-listen_port="${1:? local listen port}"
-target_host="${2:? target host from footstool}"
-target_port="${3:? target port}"
-footstool_host="${4:? footstool host}"
-shift 4
-
-ssh -f -N -L "${listen_port}:${target_host}:${target_port}" "${footstool_host}" ${@}
