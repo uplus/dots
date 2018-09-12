c610fb3c5a9cc3372a6c5ea401a3abc0f6f535fc
uplus <uplus.e10@gmail.com>
2018-09-12 16:39:26 +0900

    update

--- scripts/README.md
+++
@@ -1,4 +0,0 @@
-
-* 99-script.sh
-  * /etc/NetworkManager/dispatcher.d/99-script.sh
-  * kde proxy auto configuration
---
+++ scripts/copy_from_system.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# システムのコンフィグをバクアップする
+
+cp /etc/NetworkManager/dispatcher.d/99-proxy-autoconfig.sh .
