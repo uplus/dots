d09a0911468bf39cc0cc01917f7fb131ccc4068f
uplus <uplus.e10@gmail.com>
2018-12-05 23:58:06 +0900

    update

--- utils-old/ssh4sshfs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# http://d.hatena.ne.jp/hirose31/20110712/1310457485
-
-exec ssh -o ControlPath=none -C -c arcfour128,arcfour256,arcfour,aes128-cbc,3des-cbc,blowfish-cbc,cast128-cbc,aes192-cbc,aes256-cbc "$@"
