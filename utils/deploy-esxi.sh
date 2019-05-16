086b67eda8decc485b1b21e4c25bd2b80d3179ad
uplus <uplus.e10@gmail.com>
2019-05-16 15:36:02 +0900

    update

--- utils/deploy-esxi.sh
+++ utils/deploy-esxi.sh
@@ -10,6 +10,6 @@ vmx_name="$(ls "${base_vm_dir}/" | grep -E 'vmx$')"
 
 mkdir -p "${new_vm_name}"
 vmkfstools -i "${base_vm_dir}/${vmdk_name}" -d thin "${new_vm_name}/${vmdk_name}"
-find "${base_vm_dir}/" -not -name "*.vmdk" -exec cp {} "${new_vm_name}/" \;
+find "${base_vm_dir}/" -not -name "*.vmdk" -exec cp -i '{}' "${new_vm_name}/" \;
 
 vim-cmd solo/registervm "${datastore_dir}/${new_vm_name}/${vmx_name}" "${new_vm_name}"
